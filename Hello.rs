faec632e67a57c8c862819f73ede3911124ab8f0
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2024-09-02 15:02:57 -0300

    Add files via upload
    up files of the project

---
+++ Hello.rs
@@ -0,0 +1,11 @@
+fn
+
+main(){
+println!("Caranga says hy low yeeah to TutorialsPoint !!!");
+
+//placing holders experiment
+println!("My friends is {0}.{0} loves {1}, main activity of {0}","Vic","investing");
+
+
+}
+
